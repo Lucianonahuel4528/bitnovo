9e50a5ac5a6b8aee003bc7d39f6d4a15b7f02370
Luciano <lucianonahuel4528@gmail.com>
2024-11-07 00:37:42 -0300

    Fix en public

--- navigation/Public.js
+++ navigation/Public.js
@@ -4,6 +4,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 //Screens
 import CurrencySelector from '../src/screens/CurrencySelector';
 import Import from '../src/screens/Import';
+import PaymentRequest from '../src/screens/PaymentRequest';
 
 
 const StackPublic = createNativeStackNavigator();
@@ -28,54 +29,16 @@ export default function Public() {
           headerShown: false,
         }}
       />
-      {/* <StackPublic.Screen
-        name="Registro"
-        component={Registration}
+        <StackPublic.Screen
+        name="PaymentRequest"
+        component={PaymentRequest}
         options={{
-          title: 'Registro',
+          title: 'PaymentRequest',
           headerBackVisible: false,
           headerShown: false,
         }}
       />
-      <StackPublic.Screen
-        name="Matchs"
-        component={Matchs}
-        options={{
-          title: 'Mis Matchs',
-          headerBackVisible: false,
-          headerShown: false,
-        }}
-      />
-      <StackPublic.Screen
-        name="Habilidades"
-        component={AbilitiesScreen}
-        options={{
-          headerLeft: () => (
-            <Image
-              style={{width: 55, height: 55, margin: 2}}
-              source={require('../../src/images/CuatroDeCuatro.png')}
-            />
-          ),
-          headerBackVisible: false,
-          headerTitleAlign: 'center',
-        }}
-      />
-      <StackPublic.Screen
-        name="Offers"
-        component={OffersScreen}
-        // options={{headerShown: false}}
-      />
-      <StackPublic.Screen
-        name="Offer"
-        component={OfferScreen}
-        options={{headerShown: false}}
-      />
-      <StackPublic.Screen
-        name="Chat"
-        component={ChatScreen}
-        options={{headerShown: false}}
-
-      /> */}
+     
     </StackPublic.Navigator>
   );
 }
